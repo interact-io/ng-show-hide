1cad6ec5ea98e011d8f27142ea9982f7cc27eb1a
vuduytuananh <vuduytuananh@github.com>
2019-02-18 16:42:56 +0100

    add module to export

---
+++ src/lib/ng-show-hide.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { NgShowHideDirective } from './ng-show-hide.directive';
+@NgModule({
+  declarations: [NgShowHideDirective],
+  exports: [NgShowHideDirective]
+})
+export class NgShowHideModule {}
